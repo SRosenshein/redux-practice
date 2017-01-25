5452a30d54d97083d4cbffb5079d359af5970d28
Sam Rosenshein <srosenshein1@gmail.com>
2017-01-25 17:59:07 -0500

    Book List Practice Example

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import Books from './books';
+import ActiveBook from './active_book';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: Books,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
