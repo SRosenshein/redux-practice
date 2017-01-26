dc76e44241c5f1f745e7f9a3f2dc3179e40ca14d
Sam Rosenshein <srosenshein1@gmail.com>
2017-01-25 21:07:20 -0500

    adds redux promise

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '58fc17d83822b317e2982a081eaa35b4';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
