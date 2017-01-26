4ecea90df745869878a61b6d73f80c03a8b2c51f
Sam Rosenshein <srosenshein1@gmail.com>
2017-01-26 18:57:29 -0500

    adds redux form

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = { all: [], post: null };
+
+import {FETCH_POSTS} from '../actions/index';
+
+export default function(state = INITIAL_STATE, action) {
+	switch(action.type) {
+		case FETCH_POSTS:
+			return { ...state, all: action.payload.data };
+		default:
+			return state;
+	}
+}
