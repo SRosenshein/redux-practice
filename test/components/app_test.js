679326e8307f8399b983a777db0a7c4cd3279727
Sam Rosenshein <srosenshein1@gmail.com>
2017-03-21 13:27:18 -0400

    initial

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,1 @@
-import { renderComponent , expect } from '../test_helper';
-import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
-
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
-
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
-});
